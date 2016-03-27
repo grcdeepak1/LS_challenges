6b3c249fd9027a0b986d8934ec1e49295ced336b
Deepak <grc.deepak@gmail.com>
2016-03-27 11:31:00 -0700

    refactored saddle points

--- saddle_points.rb
+++ saddle_points.rb
@@ -18,24 +18,42 @@ class Matrix
   end
 
   def saddle_points
-  	saddle_points = []
-		rows.each_with_index do |row, row_idx|
-			max_with_index = find_max_with_index(row)
-			max_with_index.each do |num, col_idx|
-				min_with_index = columns[col_idx].enum_for(:each_with_index).min
-				if num == min_with_index.first
-       		saddle_points << [row_idx, col_idx]
-				end
-			end
-		end
-		saddle_points
+  	row_max = row_max_points
+  	col_min = col_min_points
+  	saddle = row_max & col_min
 	end
 
   private
+  def row_max_points
+  	row_max = []
+	  rows.each_with_index do |row, row_idx|
+		  find_max_with_index(row).each { |n, col_idx| row_max << [row_idx, col_idx] }
+		end
+		row_max
+	end
+
+  def col_min_points
+  	col_min = []
+  	columns.each_with_index do |col, col_idx|
+			find_min_with_index(col).each { |n, row_idx| col_min << [row_idx, col_idx] }
+		end
+		col_min
+	end
+
 	def find_max_with_index(arr)
 		ret_arr = []
 		arr.each_with_index do |num, i|
 		    if num == arr.max
+		    	ret_arr << [num, i]
+		    end
+		end
+		ret_arr
+	end
+
+	def find_min_with_index(arr)
+		ret_arr = []
+		arr.each_with_index do |num, i|
+		    if num == arr.min
 		    	ret_arr << [num, i]
 		    end
 		end
@@ -52,5 +70,4 @@ class Matrix
 		end
 		cols
   end
-
 end
