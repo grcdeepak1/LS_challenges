1fc0c7be3f4dc55b24fdd93088fa850b418bb3f6
Deepak <grc.deepak@gmail.com>
2016-03-27 09:48:53 -0700

    saddle points

---
+++ saddle_points.rb
@@ -0,0 +1,56 @@
+require 'pry'
+class Matrix
+
+	attr_reader :rows, :columns
+
+	def initialize(str)
+		@matrix = str.split("\n").map { |r| r.split(" ") }
+		@rows = @matrix.each { |r| r.collect!(&:to_i) }
+		@columns =  get_cols_from_rows(@rows)
+	end
+
+  def rows
+  	@rows
+  end
+
+  def columns
+  	@columns
+  end
+
+  def saddle_points
+  	saddle_points = []
+		rows.each_with_index do |row, row_idx|
+			max_with_index = find_max_with_index(row)
+			max_with_index.each do |num, col_idx|
+				min_with_index = columns[col_idx].enum_for(:each_with_index).min
+				if num == min_with_index.first
+       		saddle_points << [row_idx, col_idx]
+				end
+			end
+		end
+		saddle_points
+	end
+
+  private
+	def find_max_with_index(arr)
+		ret_arr = []
+		arr.each_with_index do |num, i|
+		    if num == arr.max
+		    	ret_arr << [num, i]
+		    end
+		end
+		ret_arr
+	end
+
+  def get_cols_from_rows(rows)
+		cols = []
+		cols_size = rows[0].size
+		(0..cols_size-1).each do |i|
+		  col = []
+		  rows.each { |row| col << row[i] }
+		  cols << col
+		end
+		cols
+  end
+
+end
